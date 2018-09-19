f6824e0d29dd2619e55aceddc5458f9205d8cdb8
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-09-19 22:54:59 +0000

    still trying to connect items with owners

--- routes/admin.js
+++ routes/admin.js
@@ -4,36 +4,58 @@ const bcrypt = require('bcryptjs');
 
 module.exports = (knex) => {
 
-  // REGISTRATION
+  // ********************** ALL AVAILABLE ADMIN PATHS - GET REQUESTS ************************
+
+  // ADMIN PANEL HOME PAGE
+  router.get("/panel", (req, res) => {
+    if (req.session.user_id) {
+      res.render("admin");
+    } else {
+      res.redirect("/")
+    }
+  });
+
+  // ADMIN REGISTRATION
   router.get("/register", (req, res) => {
     res.status(200);
     res.render("register");
   });
 
-  // LOGIN
+  // ADMIN LOGIN
   router.get("/login", (req, res) => {
     res.render("login")
   });
-  // ADMIN PANEL HOME PAGE
-  router.get("/panel", (req, res) => {
-    if(req.session.user_id){
-    res.render("admin");
-  } else {
-    res.redirect("/")
-  }
-  });
 
-  // ADD TO CATELOGUE PAGE
+  // CREATE ITEMS FOR CLIENT CATALOGUE
   router.get("/add", (req, res) => {
-    if(req.session.user_id){
-    res.render("create_item");
-  } else {
-    res.redirect("/")
-  }
+    if (req.session.user_id) {
+
+      res.render("create_item");
+    } else {
+      res.redirect("/")
+    }
   });
 
-  // *********** REGISTRATION AND VALIDATION  *************
-    router.post('/register', (req, res) => {
+  // ADMIN ITEM MANAGEMENT
+  router.get("/items", (req, res) => {
+    if (req.session.user_id) {
+      knex("items")
+      .select("*")
+      .then((data) => {
+        console.log(data)
+        res.json("ITEM INFO LIST: ", data)
+      })
+      .then = () => {
+        res.render("admin_item_catelogue")
+      }
+    } else {
+      res.redirect("/")
+    }
+  });
+
+  // ********************** REGISTRATION - POST REQUESTS  ************************
+
+  router.post('/register', (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password
@@ -54,71 +76,75 @@ module.exports = (knex) => {
               req.session.user_id = id[0];
               res.render("admin");
               return;
-            }).catch(function (error) {
+            }).catch(function(error) {
               console.error('Error: Inserting the user', error)
             });
         }
-      }).catch(function (error) {
+      }).catch(function(error) {
         console.error('Error: The user already Exists', error)
       });
   })
 
-  // *********** LOGIN AND VALIDATION  *************
+
+  // ********************** LOGIN - POST  ************************
+
   router.post("/login", (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
     console.log(username + password)
     knex.select('*')
-  .from('owner')
-  .where('username', '=', req.body.username)
-  .then(function (data) {
-    if (bcrypt.compareSync(req.body.password, data[0].password)) {
-      console.log('results is', data);
-      req.session.user_id = data[0].id;
-      res.render("admin");
-      return;
+      .from('owner')
+      .where('username', '=', req.body.username)
+      .then(function(data) {
+        if (bcrypt.compareSync(req.body.password, data[0].password)) {
+          console.log('results is', data);
+          req.session.user_id = data[0].id;
+          res.render("admin");
+          return;
         } else {
           res.render("register");
           return;
-    }
-  }).catch(function (error) {
+        }
+      }).catch(function(error) {
         console.error("Password is incorrect : " + error)
-});
-})
-
-// *********** LOGOUT  *************
-router.post('/logout', (req, res) => {
-req.session = null;
-res.redirect("/")
-})
-
-
-// *********** CREATING MENU ITEMS  *************
-// Add a new menu item
-router.post('/add', (req, res) => {
-  res.status(200);
-  const name = req.body.name;
-  const description = req.body.description;
-  const item_price = parseFloat(req.body.price);
-  const photo_url = req.body.photo_url;
-  console.log(name)
-  return (
-  knex("items")
-  .insert({
-    name: req.body.name,
-    description: req.body.description,
-    price: parseFloat(req.body.price),
-    photo_URL: req.body.photo_url,
+      });
   })
-)
-  .then(() => {
-    console.log("created a new item");
+
+  // *********** LOGOUT - POST REQUEST  *************
+
+  router.post('/logout', (req, res) => {
+    req.session = null;
+    res.redirect("/")
   })
-  .catch((err) => {
-    console.log(err);
+
+
+  // *********** CREATING ITEMS FOR CLIENT CATELOGUE - POST REQUEST  *************
+  // Add a new menu item
+  router.post('/add', (req, res) => {
+    res.status(200);
+    const name = req.body.name;
+    const description = req.body.description;
+    const item_price = parseFloat(req.body.price);
+    const photo_url = req.body.photo_url;
+    console.log(name)
+    return (
+        knex("items")
+        .insert({
+          name: req.body.name,
+          description: req.body.description,
+          price: parseFloat(req.body.price),
+          photo_URL: req.body.photo_url,
+        })
+      )
+      .then(() => {
+        console.log("created a new item" + user_id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res.redirect("/admin/panel");
   });
-  res.redirect("/admin/panel");
-});
+
   return router;
 }
