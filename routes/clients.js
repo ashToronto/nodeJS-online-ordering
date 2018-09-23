daed02d403119c1e17fe2d1d12a1df44612077e4
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-09-23 19:46:22 +0000

    rendering all items created by all admins on shopper catelogue

--- routes/clients.js
+++ routes/clients.js
@@ -2,9 +2,13 @@ const express         = require("express");
 const router          = express.Router();
 
 module.exports = (knex) => {
-
   router.get("/shoppers", (req, res) => {
-    res.render("clients");
+    knex("items")
+    .select("*")
+    .then(data => {
+      const templateVars = {data: data}
+      res.render("clients", templateVars);
+    })
   });
 
   return router;
