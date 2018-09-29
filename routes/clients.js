a29b8305436ac0f5f837976427106b3014eb73c4
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-09-29 20:38:35 +0000

    cleaned up css, add to cart button added and jquerry initiated

--- routes/clients.js
+++ routes/clients.js
@@ -2,6 +2,8 @@ const express         = require("express");
 const router          = express.Router();
 
 module.exports = (knex) => {
+
+  // SHOPPER CATELOGUE
   router.get("/shoppers", (req, res) => {
     knex("items")
     .select("*")
