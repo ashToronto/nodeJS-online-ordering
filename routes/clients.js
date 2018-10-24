e5d803a8a233d5913bce157410e3ddd1d56fc116
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-10-24 18:26:31 +0000

    checkout summary displays items from cart

--- routes/clients.js
+++ routes/clients.js
@@ -13,14 +13,24 @@ module.exports = (knex) => {
     })
   });
 
+const global_item_db = [];
+// NEED TO PERSIST DATA FROM GET AND POST SO NEW PAGE CAN RE RENDER IT
   router.get("/shoppers/checkout", (req, res) => {
-    res.render('clients_checkout')
+    const templateVars = {
+      item_summary_name: global_item_db[0].items.item_name,
+      items_summary_quantity: global_item_db[0].items.items_quantity,
+      item_summary_price: global_item_db[0].items.item_price,
+      checkout_total: global_item_db[0].total_price
+    }
+    console.log(global_item_db[0])
+    res.render("clients_checkout", templateVars)
   });
 
   router.post("/shoppers/checkout", (req, res) => {
-    const product = req.body.items.items_quantity
-    console.log(req.body)
-    res.redirect("/shoppers/checkout")
+    const products = req.body
+    //Push items into global array to render on new page
+    global_item_db.push(products)
+    res.redirect("shoppers/checkout")
   });
 
   return router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express         = require("express");
-const PORT            = 5000;
+const PORT            = 3000;
 const bodyParser      = require("body-parser");
 const cookieSession   = require("cookie-session");
 const bcrypt          = require('bcryptjs');
